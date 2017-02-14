3acc54994ceba0acf8077b6d536d59e1bd810f2c
simon <simon@localhost.localdomain>
2017-02-14 11:51:13 +0800

    转义序列

---
+++ DataType/sizeof.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    cout<< "bool:\t\t"<<sizeof(bool) <<endl;
+    cout<< "char:\t\t"<<sizeof(char) <<endl;
+    cout<< "short int:\t"<<sizeof(short int) <<endl;
+    cout<< "int:\t\t"<<sizeof(int) <<endl;
+    cout<< "long int:\t"<<sizeof(long int) <<endl;
+    cout<< "float:\t\t"<<sizeof(float) <<endl;
+    cout<< "doublt:\t\t"<<sizeof(double) <<endl;
+    cout<< "long double:\t"<<sizeof(long double) <<endl;
+    return 0;
+}
+
