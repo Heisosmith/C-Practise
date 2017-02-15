1a19a866a5f29c4d21a30ceef7b69835543adeb2
simon <simon@localhost.localdomain>
2017-02-15 10:55:42 +0800

    for loop

--- ControlFlow/for.cpp
+++ ControlFlow/for.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(){
@@ -9,6 +10,25 @@ int main(){
         for(j=1;j<=i;j++)
             s = s + 1;
     cout<< "s = "<< s <<endl;
+
+    //求（100,200）间的所有质数
+    int k,n,sign;
+    //n+=2,由已知条件减少运算量
+    for(n=101;n<200;n+=2){
+        k = sqrt(n);
+        //采用标志位的方法
+        for(i=2;i<k;i++){
+            sign = 1;
+            if(n%i==0){
+                sign = 0;
+                break;
+            }
+        }
+        if(sign)
+            cout<< n <<" ";
+    }
+    cout<<endl;
+
     return 0;
 }
 
