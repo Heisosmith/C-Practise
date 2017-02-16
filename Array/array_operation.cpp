a8ac22c6d446a669e9f5ccc2e9f34cdc230cd278
simon <simon@localhost.localdomain>
2017-02-16 17:36:44 +0800

    array_operation

--- Array/array_operation.cpp
+++ Array/array_operation.cpp
@@ -1,9 +1,9 @@
-//计算并输出每行元素之和及每列元素之和
-
 #include <iostream>
 using namespace std;
 
 int main(){
+    
+    //计算并输出每行元素之和及每列元素之和
     int a[5][5],
         i,j,
         cnt  =1,
@@ -28,6 +28,33 @@ int main(){
         }
             cout<< rsum<<"\t"<< csum<<endl;
     }
+
+    cout<<endl;
+
+    //输出一个二维数组中的鞍点（某元素在该行最大，该列最小）
+    int max,
+        sub,
+        flag = 1;
+    for(i=0;i<5;i++){
+        max = a[i][0];
+        for(j=0;j<5;j++){
+            if( a[i][j] >max)
+                max = a[i][j];
+                sub = j;
+        }
+        for(j=0;j<5;j++){
+            //Debug:if-else错误，会输出全列
+            //使用举反例的方法，需要结合标志位一起使用
+            if( a[j][sub] <max){
+                flag = 0; 
+                break;
+            }
+        }
+        if(flag)
+            cout<< a[i][sub] <<endl;
+            
+    }
+
     return 0;
 }
 
